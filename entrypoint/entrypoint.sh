f566422d25797f7b611aff7ec6d62f7c5e68ebcf
Mauricio Nunes <mnunes@vialink.com.br>
2021-03-31 01:59:11 -0300

    wait path fix

--- entrypoint/entrypoint.sh
+++ entrypoint/entrypoint.sh
@@ -4,12 +4,13 @@ cd /monitor-de-metas
 git pull
 chown -R www-data: /monitor-de-metas
 
-wget https://github.com/ufoscout/docker-compose-wait/releases/download/2.8.0/wait /wait
+wget https://github.com/ufoscout/docker-compose-wait/releases/download/2.8.0/wait waitingfor
+cp waitingfor /usr/local/bin
 
 sed -i -e "s/root %%NGINX_ROOT%%;/root \/monitor-de-metas;/g" /etc/nginx/sites-available/default.conf
 
-sh -c "/wait && wp-cli config create --dbhost=${DBHOST} --dbname=${DBNAME} --dbuser=${DBUSER} --dbpass=${DBPASS} --dbprefix=${DBPREFIX} --locale=pt_BR --path=/monitor-de-metas --allow-root"
-sh -c "/wait && wp-cli core install --url=${URL} --title=${TITLE} --admin_user=${ADMUSER} --admin_password=${ADMPASS} --admin_email=${ADMMAIL} --path=/monitor-de-metas --allow-root"
+sh -c "waitingfor && wp-cli config create --dbhost=${DBHOST} --dbname=${DBNAME} --dbuser=${DBUSER} --dbpass=${DBPASS} --dbprefix=${DBPREFIX} --locale=pt_BR --path=/monitor-de-metas --allow-root"
+sh -c "waitingfor && wp-cli core install --url=${URL} --title=${TITLE} --admin_user=${ADMUSER} --admin_password=${ADMPASS} --admin_email=${ADMMAIL} --path=/monitor-de-metas --allow-root"
 
 cd /monitor-de-metas/wp-content/themes/pdm-andamento/
 
